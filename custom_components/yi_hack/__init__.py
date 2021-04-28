26ef0964d52ff60fd4ef276b897b7853089dfd18
Andrey F. Kupreychik <foxel@quickfox.ru>
2021-04-28 17:04:26 +0700

    Correct binary sensors

--- custom_components/yi_hack/__init__.py
+++ custom_components/yi_hack/__init__.py
@@ -10,16 +10,24 @@ from .config import get_mqtt_conf, get_system_conf
 from .const import (
     ALLWINNER,
     ALLWINNERV2,
+    CONF_AI_HUMAN_DETECTION_START_MSG,
+    CONF_AI_HUMAN_DETECTION_STOP_MSG,
+    CONF_BABY_CRYING_MSG,
+    CONF_BIRTH_MSG,
     CONF_HACK_NAME,
+    CONF_MOTION_START_MSG,
+    CONF_MOTION_STOP_MSG,
     CONF_MQTT_PREFIX,
     CONF_RTSP_PORT,
     CONF_SERIAL,
+    CONF_SOUND_DETECTION_MSG,
     CONF_TOPIC_AI_HUMAN_DETECTION,
     CONF_TOPIC_BABY_CRYING,
     CONF_TOPIC_MOTION_DETECTION,
     CONF_TOPIC_MOTION_DETECTION_IMAGE,
     CONF_TOPIC_SOUND_DETECTION,
     CONF_TOPIC_STATUS,
+    CONF_WILL_MSG,
     DOMAIN,
 )
 
@@ -38,15 +46,30 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     mqtt = await hass.async_add_executor_job(get_mqtt_conf, entry)
 
     if conf is not None and mqtt is not None:
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_RTSP_PORT: conf[CONF_RTSP_PORT]})
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_MQTT_PREFIX: mqtt[CONF_MQTT_PREFIX]})
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_STATUS: mqtt[CONF_TOPIC_STATUS]})
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_MOTION_DETECTION: mqtt[CONF_TOPIC_MOTION_DETECTION]})
+        updated_data = {
+            **entry.data,
+            CONF_RTSP_PORT: conf[CONF_RTSP_PORT],
+            CONF_MQTT_PREFIX: mqtt[CONF_MQTT_PREFIX],
+            CONF_TOPIC_STATUS: mqtt[CONF_TOPIC_STATUS],
+            CONF_TOPIC_MOTION_DETECTION: mqtt[CONF_TOPIC_MOTION_DETECTION],
+            CONF_TOPIC_BABY_CRYING: mqtt[CONF_TOPIC_BABY_CRYING],
+            CONF_MOTION_START_MSG: mqtt[CONF_MOTION_START_MSG],
+            CONF_MOTION_STOP_MSG: mqtt[CONF_MOTION_STOP_MSG],
+            CONF_BABY_CRYING_MSG: mqtt[CONF_BABY_CRYING_MSG],
+            CONF_BIRTH_MSG: mqtt[CONF_BIRTH_MSG],
+            CONF_WILL_MSG: mqtt[CONF_WILL_MSG],
+            CONF_TOPIC_MOTION_DETECTION_IMAGE: mqtt[CONF_TOPIC_MOTION_DETECTION_IMAGE],
+        }
         if (entry.data[CONF_HACK_NAME] == ALLWINNER) or (entry.data[CONF_HACK_NAME] == ALLWINNERV2):
-            hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_AI_HUMAN_DETECTION: mqtt[CONF_TOPIC_AI_HUMAN_DETECTION]})
-            hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_SOUND_DETECTION: mqtt[CONF_TOPIC_SOUND_DETECTION]})
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_BABY_CRYING: mqtt[CONF_TOPIC_BABY_CRYING]})
-        hass.config_entries.async_update_entry(entry, data={**entry.data, CONF_TOPIC_MOTION_DETECTION_IMAGE: mqtt[CONF_TOPIC_MOTION_DETECTION_IMAGE]})
+            updated_data.update(**{
+                CONF_TOPIC_AI_HUMAN_DETECTION: mqtt[CONF_TOPIC_AI_HUMAN_DETECTION],
+                CONF_TOPIC_SOUND_DETECTION: mqtt[CONF_TOPIC_SOUND_DETECTION],
+                CONF_AI_HUMAN_DETECTION_START_MSG: mqtt[CONF_AI_HUMAN_DETECTION_START_MSG],
+                CONF_AI_HUMAN_DETECTION_STOP_MSG: mqtt[CONF_AI_HUMAN_DETECTION_STOP_MSG],
+                CONF_SOUND_DETECTION_MSG: mqtt[CONF_SOUND_DETECTION_MSG],
+            })
+
+        hass.config_entries.async_update_entry(entry, data=updated_data)
 
         for component in PLATFORMS:
             hass.async_create_task(
--- custom_components/yi_hack/binary_sensor.py
+++ custom_components/yi_hack/binary_sensor.py
@@ -2,25 +2,39 @@
 import logging
 
 from homeassistant.components import mqtt
-from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.binary_sensor import (
+    DEVICE_CLASS_CONNECTIVITY,
+    DEVICE_CLASS_MOTION,
+    DEVICE_CLASS_SOUND,
+    BinarySensorEntity,
+)
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_MAC, CONF_NAME
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import event
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 
 from .const import (
     ALLWINNER,
     ALLWINNERV2,
+    CONF_AI_HUMAN_DETECTION_START_MSG,
+    CONF_AI_HUMAN_DETECTION_STOP_MSG,
+    CONF_BABY_CRYING_MSG,
+    CONF_BIRTH_MSG,
     CONF_HACK_NAME,
+    CONF_MOTION_START_MSG,
+    CONF_MOTION_STOP_MSG,
     CONF_MQTT_PREFIX,
     CONF_SERIAL,
+    CONF_SOUND_DETECTION_MSG,
     CONF_TOPIC_AI_HUMAN_DETECTION,
     CONF_TOPIC_BABY_CRYING,
     CONF_TOPIC_MOTION_DETECTION,
     CONF_TOPIC_SOUND_DETECTION,
     CONF_TOPIC_STATUS,
+    CONF_WILL_MSG,
     DEFAULT_BRAND,
     DOMAIN,
-    MSTAR,
 )
 
 ICON = "mdi:update"
@@ -28,71 +42,114 @@ ICON = "mdi:update"
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, config, async_add_entities):
+async def async_setup_entry(_hass: HomeAssistant, config: ConfigEntry, async_add_entities):
     """Set up MQTT motion detection sensor."""
+    entities = [
+        YiMQTTBinarySensor(config, CONF_TOPIC_STATUS),
+        YiMQTTBinarySensor(config, CONF_TOPIC_MOTION_DETECTION),
+        YiMQTTBinarySensor(config, CONF_TOPIC_BABY_CRYING)
+    ]
 
-    if (config.data[CONF_HACK_NAME] == DEFAULT_BRAND) or (config.data[CONF_HACK_NAME] == MSTAR):
-        async_add_entities(
+    if (config.data[CONF_HACK_NAME] == ALLWINNER) or (config.data[CONF_HACK_NAME] == ALLWINNERV2):
+        entities.append(
             [
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_STATUS),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_MOTION_DETECTION),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_BABY_CRYING)
+                YiMQTTBinarySensor(config, CONF_TOPIC_AI_HUMAN_DETECTION),
+                YiMQTTBinarySensor(config, CONF_TOPIC_SOUND_DETECTION),
             ]
         )
-    elif (config.data[CONF_HACK_NAME] == ALLWINNER) or (config.data[CONF_HACK_NAME] == ALLWINNERV2):
-        async_add_entities(
-            [
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_STATUS),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_MOTION_DETECTION),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_AI_HUMAN_DETECTION),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_SOUND_DETECTION),
-                YiMQTTBinarySensor(hass, config, CONF_TOPIC_BABY_CRYING)
-            ]
-        )
+
+    async_add_entities(entities)
 
 
 class YiMQTTBinarySensor(BinarySensorEntity):
     """Representation of a motion detection sensor that is updated via MQTT."""
 
-    def __init__(self, hass, config, sensor_type):
+    def __init__(self, config: ConfigEntry, sensor_type):
         """Initialize the sensor."""
         self._state = False
         self._device_name = config.data[CONF_NAME]
         self._serial_number = config.data[CONF_SERIAL]
         self._mac = config.data[CONF_MAC]
-        self._updated = None
         self._mqtt_subscription = None
+        self._delay_listener = None
+        self._payload_off = None
+        self._off_delay = None
 
         if sensor_type == CONF_TOPIC_STATUS:
             self._name = self._device_name + "_status"
             self._unique_id = self._device_name + "_bsst"
             self._state_topic = config.data[CONF_MQTT_PREFIX] + "/" + config.data[CONF_TOPIC_STATUS]
+            self._payload_on = config.data[CONF_BIRTH_MSG]
+            self._payload_off = config.data[CONF_WILL_MSG]
+            self._device_class = DEVICE_CLASS_CONNECTIVITY
         elif sensor_type == CONF_TOPIC_MOTION_DETECTION:
             self._name = self._device_name + "_motion_detection"
             self._unique_id = self._device_name + "_bsmd"
             self._state_topic = config.data[CONF_MQTT_PREFIX] + "/" + config.data[CONF_TOPIC_MOTION_DETECTION]
+            self._payload_on = config.data[CONF_MOTION_START_MSG]
+            self._payload_off = config.data[CONF_MOTION_STOP_MSG]
+            self._device_class = DEVICE_CLASS_MOTION
         elif sensor_type == CONF_TOPIC_AI_HUMAN_DETECTION:
             self._name = self._device_name + "_ai_human_detection"
             self._unique_id = self._device_name + "_bsah"
             self._state_topic = config.data[CONF_MQTT_PREFIX] + "/" + config.data[CONF_TOPIC_AI_HUMAN_DETECTION]
+            self._payload_on = config.data[CONF_AI_HUMAN_DETECTION_START_MSG]
+            self._payload_off = config.data[CONF_AI_HUMAN_DETECTION_STOP_MSG]
+            self._device_class = DEVICE_CLASS_MOTION
         elif sensor_type == CONF_TOPIC_SOUND_DETECTION:
             self._name = self._device_name + "_sound_detection"
             self._unique_id = self._device_name + "_bssd"
             self._state_topic = config.data[CONF_MQTT_PREFIX] + "/" + config.data[CONF_TOPIC_SOUND_DETECTION]
+            self._payload_on = config.data[CONF_SOUND_DETECTION_MSG]
+            self._off_delay = 60
+            self._device_class = DEVICE_CLASS_SOUND
         elif sensor_type == CONF_TOPIC_BABY_CRYING:
             self._name = self._device_name + "_baby_crying"
             self._unique_id = self._device_name + "_bsbc"
             self._state_topic = config.data[CONF_MQTT_PREFIX] + "/" + config.data[CONF_TOPIC_BABY_CRYING]
+            self._payload_on = config.data[CONF_BABY_CRYING_MSG]
+            self._off_delay = 60
+            self._device_class = DEVICE_CLASS_SOUND
+        else:
+            raise RuntimeError("Unknown sensor type")
 
     async def async_added_to_hass(self):
         """Subscribe to MQTT events."""
 
         @callback
+        def off_delay_listener(now):
+            """Switch device off after a delay."""
+            self._delay_listener = None
+            self._state = False
+            self.async_write_ha_state()
+
+        @callback
         def message_received(msg):
             """Handle new MQTT messages."""
-            data = msg.payload
+            payload = msg.payload
 
-            self._state = data
+            if payload == self._payload_on:
+                self._state = True
+            elif payload == self._payload_off:
+                self._state = False
+            else:  # Payload is not for this entity
+                _LOGGER.info(
+                    "No matching payload found for entity: %s with state topic: %s. Payload: '%s'",
+                    self._name,
+                    self._state_topic,
+                    payload,
+                )
+                return
+
+            if self._delay_listener is not None:
+                self._delay_listener()
+                self._delay_listener = None
+
+            if self._state and self._off_delay is not None:
+                self._delay_listener = event.async_call_later(
+                    self.hass, self._off_delay, off_delay_listener
+                )
+
             self.async_write_ha_state()
 
         self._mqtt_subscription = await mqtt.async_subscribe(
@@ -110,7 +167,7 @@ class YiMQTTBinarySensor(BinarySensorEntity):
         return self._name
 
     @property
-    def state(self):
+    def is_on(self):
         """Return the state of the sensor."""
         return self._state
 
@@ -125,19 +182,9 @@ class YiMQTTBinarySensor(BinarySensorEntity):
         return self._unique_id
 
     @property
-    def icon(self):
+    def device_class(self):
         """Return the icon to use in the frontend."""
-        return ICON
-
-    @property
-    def device_info(self):
-        """Return device specific attributes."""
-        return {
-            "name": self._name,
-            "identifiers": {(DOMAIN, self._serial_number)},
-            "manufacturer": DEFAULT_BRAND,
-            "model": DOMAIN,
-        }
+        return self._device_class
 
     @property
     def device_info(self):
